56a9c05a3f4321e767cdeb30b906535ed83b926b
r-kabir <taposkabir@yahoo.com>
2024-10-26 18:34:48 +0600

    react installed and created a hydrated component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrolook.netlify.app"
+  site: "https://astrolook.netlify.app",
+  integrations: [react()]
 });
